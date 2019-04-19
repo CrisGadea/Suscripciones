59cc454027468b35f6a9c745298f67291b924188
CrisGadea <cristianhernangadea@gmail.com>
2019-04-19 03:21:53 -0300

    cambios realizados a routes, settings, dependencies y middleware y otros archivos agregados al proyecto

--- src/routes.php
+++ src/routes.php
@@ -2,13 +2,59 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Slim\App;
 
 // Routes
+/**
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->group('/users/{id:[0-9]+}', function (App $app) {
+    $app->map(['GET', 'DELETE', 'PATCH', 'PUT'], '', function ($request, $response, $args) {
+        // Find, delete, patch or replace user identified by $args['id']
+    })->setName('user');
+    $app->get('/reset-password', function ($request, $response, $args) {
+        // Route for /users/{id:[0-9]+}/reset-password
+        // Reset the password for user identified by $args['id']
+    })->setName('user-password-reset');
+});
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+$app->group($pattern, function () {})
+    ->add(new SimpleTokenAuthentication($app->getContainer(), $options));
+*/
+$app = new App();
+$app->group('/mock', function(App $app){
+    $app->get('/user/{id}',function(Request $request, Response $response, $args){
+        return $response->withJson($args,200);
+    });
+
+    
+    $app->get('/User',function(Request $request, Response $response, array $args){
+        return $response->withJson("Hola mundo");
+    });
+    $app->post('/User',function(Request $request, Response $response, array $args){
+        return $response->withJson("Hola mundo");
+    });
+    $app->put('/User/{id}',function(Request $request, Response $response, array $args){
+        return $response->withJson("Hola mundo");
+    });
+    $app->delete('/User/{id}',function(Request $request, Response $response, array $args){
+        return $response->withJson("Hola mundo");
+    });   
+});
+
+$app->group('/v1', function(App $app){
+    $app->get('/user/{id}',function(Request $request, Response $response, $args){
+        return $response->withJson($args,200);
+    });
+    $app->get('/User',function(Request $request, Response $response, array $args){
+        return $response->withJson("Hola mundo");
+    });
+    $app->post('/User',function(Request $request, Response $response, array $args){
+        return $response->withJson("Hola mundo");
+    });
+    $app->put('/User/{id}',function(Request $request, Response $response, array $args){
+        return $response->withJson("Hola mundo");
+    });
+    $app->delete('/User/{id}',function(Request $request, Response $response, array $args){
+        return $response->withJson("Hola mundo");
+    });
 });
--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,21 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+	 // database connection details         
+        "db" => [            
+             "host" => "your-host",             
+             "dbname" => "your-database-name",             
+             "user" => "your-db-username",            
+             "pass" => "your-db-password"        
+         ],
+	
+
+        // jwt settings
+        "jwt" => [
+            'secret' => 'supersecretkeyyoushouldnotcommittogithub'
+        ]
+
     ],
+	
 ];
