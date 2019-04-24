a2c15445e39870911f9ee02ada29fa6b4d3a86c9
nacho gomez <nacho.gomezcc@outlook.com>
2019-04-24 15:44:33 -0300

    1111

---
+++ tests/RoutesTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Functional;
+
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Http\Environment;
+
+
+
+class RoutesTest extends TestCase {
+    
+    public function user(){
+        $this->assertEquals();
+    }
+}
