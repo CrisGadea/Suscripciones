2935717b2f63347396c4b6b52e53cda923eac753
CrisGadea <cristianhernangadea@gmail.com>
2019-04-24 16:45:39 -0300

    se agregaron tests de los mock

--- tests/RoutesTest.php
+++ tests/RoutesTest.php
@@ -12,7 +12,7 @@ class RoutesTest extends TestCase
 
     public function setUp()
     {
-        $this->http = new Client(['base_uri' => 'https://httpbin.org/']);
+        $this->http = new Client(['base_uri' => 'http://localhost:8080/']);
     }
 
     public function tearDown() {
@@ -21,7 +21,20 @@ class RoutesTest extends TestCase
 
     public function testGet()
 {
-    $response = $this->http->request('GET', 'mock/user-agent');
+    $response = $this->http->request('GET', 'mock/product');
+
+    $this->assertEquals(200, $response->getStatusCode());
+
+    $contentType = $response->getHeaders()["Content-Type"][0];
+    $this->assertEquals("applciation/json", $contentType);
+
+    $userAgent = json_decode($response->getBody())->{"user-agent"};
+    $this->assertRegexp('/Guzzle/', $userAgent);
+}
+
+public function testGetId()
+{
+    $response = $this->http->request('GET', 'mock/product/{id}');
 
     $this->assertEquals(200, $response->getStatusCode());
 
@@ -34,8 +47,20 @@ class RoutesTest extends TestCase
 
 public function testPut()
 {
-    $response = $this->http->request('PUT', 'user-agent', ['http_errors' => false]);
+    $response = $this->http->request('PUT', 'mock/product', ['http_errors' => false]);
 
     $this->assertEquals(405, $response->getStatusCode());
+}
+
+public function testPost()
+{
+    $response = $this->http->request('POST', 'mock/product');
+
+}
+
+public function testDelete()
+{
+    $response = $this->http->request('DELETE', 'mock/product');
+
 }
 }
