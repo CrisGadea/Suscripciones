2ec9afeea143948a9e4c8f1436c23f020bf215c1
CrisGadea <cristianhernangadea@gmail.com>
2019-05-15 21:48:36 -0300

    Incorporación y finalización de Profile, Purchase and User

--- src/routes.php
+++ src/routes.php
@@ -147,11 +147,11 @@ $app->group('/v1', function(App $app){
         return $response->withStatus(202)->withJson($product);
     });
 
-    $app->post('/product',function(Request $request, Response $response) use ($app){
+    $app->post('/product[/]',function(Request $request, Response $response) use ($app){
         $db = $app->getContainer()['db'];
         $mgProducts = $db->getCollection('products');
         $datos = $request->getParsedBody();
-        $product = $mgProducts->createDocument( 
+        $product = $mgProducts->createDocument(
             [ 
                 'name' => $datos['nombre'],
                 'price' => $datos['precio'],
@@ -179,54 +179,206 @@ $app->group('/v1', function(App $app){
         }
         return $response->withStatus(204);
     });
-/*
-    $app->get('/user/{id}',function(Request $request, Response $response, $args){
-        return $response->withStatus(200)->withJson();
+
+    $app->get('/user/{id}',function(Request $request, Response $response, $id)use($app){
+        try {
+            $db = $app->getContainer()['db'];
+            $mgUser = $db->getCollection('users');
+            $route = $request->getAttribute('route');
+            $id = $route->getArgument('id');
+            $user = $mgUser->getDocument($id);
+            if ($user === null) {
+                throw new Exception("Not found");
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            $obj = new \stdClass;
+            $obj->message = 'This user does not exists';
+            return $response->withStatus(404)->withJson( $obj);
+    
+        }    
+            return $response->withStatus(200)->withJson($user);
     });
-    $app->get('/user',function(Request $request, Response $response, array $args){
-        return $response->withStatus(200)->withJson();
+    $app->get('/user[/]',function(Request $request, Response $response, array $args) use($app){
+        $db = $app->getContainer()['db'];
+        $mgUser = $db->getCollection('users');
+        return $response->withStatus(200)->withJson($mgUser->find()->slice('users',1000)->findAll());
     });
-    $app->post('/user',function(Request $request, Response $response, array $args){
-        return $response->withStatus(201)->withJson();
+    $app->post('/user',function(Request $request, Response $response, array $args) use($app){
+         $db = $app->getContainer()['db'];
+        $mgUser = $db->getCollection('users');
+        $datos = $request->getParsedBody();
+        $user = $mgUser->createDocument(
+            [ 
+                'email' => $datos['email'],
+                'password' => $datos['password'],
+                'country' => $datos['pais']
+            ]
+        )->save(false);
+        return $response->withStatus(201)->withJson($user);
     });
-    $app->put('/user/{id}',function(Request $request, Response $response, array $args){
-        return $response->withStatus(202)->withJson();
+    $app->put('/user/{id}',function(Request $request, Response $response, $id) use($app){
+        try {
+            $db = $app->getContainer()['db'];
+            $mgUser = $db->getCollection('users');
+            $route = $request->getAttribute('route');
+            $id = $route->getArgument('id'); 
+            $data = $mgUser->getDocument($id);
+            if ($data === null) {
+                throw new Exception("Not found");
+            }
+            $datos = $request->getParsedBody();
+            $user = $data
+            ->set('email',$datos['email'])
+            ->set('password',(String)$datos['password'])
+            ->set('country',$datos['pais'])
+            ->save();
+            
+            }catch (Exception $e) {
+                    echo $e->getMessage();
+                    $obj = new \stdClass;
+                    $obj->message = 'This user cant be updated because does not exist';
+                    return $response->withStatus(404)->withJson( $obj);
+                }
+            return $response->withStatus(202)->withJson($user);
     });
-    $app->delete('/user/{id}',function(Request $request, Response $response, array $args){
-        return $response->withStatus(204);
+    $app->delete('/user/{id}',function(Request $request, Response $response, $id) use($app){
+        try {
+            $db = $app->getContainer()['db'];
+            $mgUser = $db->getCollection('users');
+            $route = $request->getAttribute('route');
+            $id = $route->getArgument('id');
+            if ($mgUser->getDocument($id)===null){
+                throw new Exception("Not found");
+            }
+            $user = $mgUser->getDocument($id)->delete(); 
+            }catch (Exception $e) {
+                echo $e->getMessage();
+                $obj = new \stdClass;
+                $obj->message = 'This user cant be deleted because does not exist';
+                return $response->withStatus(404)->withJson( $obj);
+            }
+            return $response->withStatus(204);
     });
     
+    $app->get('/purchase/{userId}',function(Request $request, Response $response, $userId) use($app){
+        try {
+            $db = $app->getContainer()['db'];
+            $mgPurchase = $db->getCollection('purchases');
+            $route = $request->getAttribute('route');
+            $userId = $route->getArgument('userId');
+            $purchase = $mgPurchase->getDocument($userId);
+            if ($purchase === null) {
+                throw new Exception("Not found");
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            $obj = new \stdClass;
+            $obj->message = 'Those purchases do not exist because the user can not be founded';
+            return $response->withStatus(404)->withJson( $obj);
+    
+        }    
+            return $response->withStatus(200)->withJson($purchase);
+    });
+    $app->get('/purchase',function(Request $request, Response $response, array $args) use($app){
+        $db = $app->getContainer()['db'];
+        $mgPurchase = $db->getCollection('purchases');
+        return $response->withStatus(200)->withJson($mgPurchase->find()->slice('purchases',1000)->findAll());
+    });*
 
-    $app->get('/purchase/{id}',function(Request $request, Response $response, $args){
-        return $response->withStatus(200)->withJson();
+    $app->post('/purchase[/]',function(Request $request, Response $response, array $args) use($app){
+        $db = $app->getContainer()['db'];
+        $mgPurchase = $db->getCollection('purchases');
+        $datos = $request->getParsedBody();
+        $purchase = $mgPurchase->createDocument(
+            [ 
+                'date' => $datos['fecha'],
+                'price' => $datos['precio'],
+                'userId' => $datos['usuario'],
+                'productId' => $datos['producto']
+            ]
+        )->save(false);
+        return $response->withStatus(201)->withJson($purchase);
     });
-    $app->get('/purchase',function(Request $request, Response $response, array $args){
-        return $response->withStatus(200)->withJson();
+/*    $app->put('/purchase/{id}',function(Request $request, Response $response, $id) use($app){ 
     });
-    $app->post('/productpurchase',function(Request $request, Response $response, array $args){
-        return $response->withStatus(201)->withJson();
+    $app->delete('/purchase/{id}',function(Request $request, Response $response, $id) use($app)   
     });
-    $app->put('/purchase/{id}',function(Request $request, Response $response, array $args){
-        return $response->withStatus(202)->withJson() ;
+*/
+    $app->get('/profile/{id}',function(Request $request, Response $response, $id) use($app){
+         try {
+            $db = $app->getContainer()['db'];
+            $mgProfile = $db->getCollection('profiles');
+            $route = $request->getAttribute('route');
+            $id = $route->getArgument('id');
+            $profile = $mgProfile->getDocument($id);
+            if ($profile === null) {
+                throw new Exception("Not found");
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            $obj = new \stdClass;
+            $obj->message = 'This profile do not exist';
+            return $response->withStatus(404)->withJson( $obj);
+    
+        }    
+            return $response->withStatus(200)->withJson($profile);
     });
-    $app->delete('/purchase/{id}',function(Request $request, Response $response, array $args){
-        return $response->withStatus(204);
+   // $app->get('/profile',function(Request $request, Response $response, array $args) use($app){   
+   // });
+    $app->post('/profile',function(Request $request, Response $response, array $args) use($app){
+        $db = $app->getContainer()['db'];
+        $mgProfile = $db->getCollection('profiles');
+        $datos = $request->getParsedBody();
+        $profile = $mgProfile->createDocument(
+            [ 
+                'userId' => $datos['usuario'],
+                'purchaseId' => $datos['compra'],
+                'name' => $datos['nombre']
+            ]
+        )->save(false);
+        return $response->withStatus(201)->withJson($profile);
     });
-
-    $app->get('/profile/{id}',function(Request $request, Response $response, $args){
-        return $response->withStatus(200)->withJson();
+    $app->put('/profile/{id}',function(Request $request, Response $response, $id) use($app){
+        try {
+            $db = $app->getContainer()['db'];
+            $mgProfile = $db->getCollection('profiles');
+            $route = $request->getAttribute('route');
+            $id = $route->getArgument('id'); 
+            $data = $mgProfile->getDocument($id);
+            if ($data === null) {
+                throw new Exception("Not found");
+            }
+            $datos = $request->getParsedBody();
+            $profile = $data
+            ->set('user',$datos['usuario'])
+            ->set('purchase',$datos['compra'])
+            ->save();
+            
+            }catch (Exception $e) {
+                    echo $e->getMessage();
+                    $obj = new \stdClass;
+                    $obj->message = 'This profile cant be updated because does not exist';
+                    return $response->withStatus(404)->withJson( $obj);
+                }
+            return $response->withStatus(202)->withJson($profile);
     });
-    $app->get('/profile',function(Request $request, Response $response, array $args){
-        return $response->withStatus(200)->withJson();
+    $app->delete('/profile/{id}',function(Request $request, Response $response, $id) use($app){
+        try {
+            $db = $app->getContainer()['db'];
+            $mgProfile = $db->getCollection('profiles');
+            $route = $request->getAttribute('route');
+            $id = $route->getArgument('id');
+            if ($mgProfile->getDocument($id)===null){
+                throw new Exception("Not found");
+            }
+            $profile = $mgProfile->getDocument($id)->delete(); 
+            }catch (Exception $e) {
+                echo $e->getMessage();
+                $obj = new \stdClass;
+                $obj->message = 'This profile cant be deleted because does not exist';
+                return $response->withStatus(404)->withJson( $obj);
+            }
+            return $response->withStatus(204);
     });
-    $app->post('/profile',function(Request $request, Response $response, array $args){
-        return $response->withStatus(201)->withJson();
-    });
-    $app->put('/profile/{id}',function(Request $request, Response $response, array $args){
-        return $response->withStatus(202)->withJson();
-    });
-    $app->delete('/profile/{id}',function(Request $request, Response $response, array $args){
-        return $response->withStatus(204);
-    });
-*/ 
 });
